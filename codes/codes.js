683ac33186ef966ed46a6a60f17aa6842a4f27a3
OMI786I <sohrawardy1998@gmail.com>
2024-04-16 17:56:24 +0600

    added deselect

--- codes/codes.js
+++ codes/codes.js
@@ -1,7 +1,14 @@
 function getValue(btn) {
   const cal = btn.getAttribute("value");
-  btn.classList.add("bg-[#1dd100]");
-  btn.classList.add("text-white");
 
+  if (!btn.classList.contains("bg-[#1dd100]")) {
+    btn.classList.add("bg-[#1dd100]");
+    btn.classList.add("text-white");
+  } else {
+    btn.classList.remove("bg-[#1dd100]");
+    btn.classList.remove("text-white");
+  }
+
+  //console.clear();
   console.log(cal);
 }
