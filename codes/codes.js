dec051fe07d15cbd917eb456f04ac569dde932ac
OMI786I <sohrawardy1998@gmail.com>
2024-04-16 18:14:56 +0600

    added value inside an array and added removing deselected element from array

--- codes/codes.js
+++ codes/codes.js
@@ -1,14 +1,21 @@
+let array = [];
+
 function getValue(btn) {
   const cal = btn.getAttribute("value");
 
   if (!btn.classList.contains("bg-[#1dd100]")) {
     btn.classList.add("bg-[#1dd100]");
     btn.classList.add("text-white");
+    console.log(cal);
+    array.push(cal);
+    console.log(array);
   } else {
     btn.classList.remove("bg-[#1dd100]");
     btn.classList.remove("text-white");
+    const index = array.indexOf(cal);
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+    console.log(array);
   }
-
-  //console.clear();
-  console.log(cal);
 }
